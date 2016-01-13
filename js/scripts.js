7b5fff18294a4e8596c340e7bb016ca18faa5c31
Nic Netzel and torrence stratton <netzeln@gmail.com>
2016-01-13 09:07:24 -0800

    Complete page function

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
 
-
+  if (age >=18) {
+    $(".over18").show();
+    alert("GO REGISTER!!");
+  } else {
+    $(".under18").show();
+  }
 
 });
